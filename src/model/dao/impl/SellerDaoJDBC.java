997d3febf2a755477fd19ba5bbb17cfa1921dd57
Diovane Pacheti <diovane.pacheti@l2code.com.br>
2022-12-27 21:20:59 -0300

    :sparkles: create findByDepartment implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -10,7 +10,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SellerDaoJDBC implements SellerDao {
 
@@ -67,6 +70,52 @@ public class SellerDaoJDBC implements SellerDao {
         return null;
     }
 
+    @Override
+    public List<Seller> findByDepartment(Department department) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try{
+
+            st = conn.prepareStatement("SELECT seller.*, department.Name as DepName\n" +
+                    "FROM seller INNER JOIN department \n" +
+                    "ON seller.DepartmentId = department.Id\n" +
+                    "WHERE DepartmentId = ? ORDER BY Name");
+
+            st.setInt(1, department.getId());
+            rs = st.executeQuery();
+
+            List<Seller> sellerList = new ArrayList<>();
+
+            //criando uma estrutura map pra nao repetir a instanciação do departamento
+            Map<Integer, Department> map = new HashMap<>();
+
+            while(rs.next()){
+
+                //Buscando um department pelo Id vindo do rs
+                Department dep = map.get(rs.getInt("DepartmentId"));
+
+                //se dep for null
+                if(dep == null){
+
+                    //instancia o Departamento
+                    dep = instantiateDepartment(rs);
+
+                    //e adiciona a chave e o valor ao Map
+                    map.put(rs.getInt("DepartmentId"), dep);
+                }
+                Seller obj = instantiateSeller(rs, dep);
+                sellerList.add(obj);
+            }
+            return sellerList;
+        }catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+    }
+
     private Department instantiateDepartment(ResultSet rs) throws SQLException{
         Department dep = new Department();
 
@@ -88,4 +137,6 @@ public class SellerDaoJDBC implements SellerDao {
 
         return obj;
     }
+
+
 }
